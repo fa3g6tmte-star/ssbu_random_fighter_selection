a12ad8564d5e46ad514cc1574acac34a6d87c89b
fa3g6tmte-star <fa3g6tmte@gmail.com>
2025-11-11 00:50:35 +0900

    script.js を更新

--- js/script.js
+++ js/script.js
@@ -183,9 +183,6 @@ randomButton.addEventListener("click", () => {
 
                 frag.appendChild(fighterBox);
 
-                // ここを追加 ↓
-                addResultClickToggle(fighterBox, i);
-
                 // 決定済みキャラに追加
                 selectedFighters.push(i);
 
@@ -202,9 +199,6 @@ randomButton.addEventListener("click", () => {
                 const fighterBox = makeFighterBox(i);
 
                 frag.appendChild(fighterBox);
-
-                // ここを追加 ↓
-                addResultClickToggle(fighterBox, i);
 
                 // 決定済みキャラに追加
                 selectedFighters.push(i);
@@ -224,36 +218,6 @@ randomButton.addEventListener("click", () => {
     
     
 });
-
-// resultList 配下のキャラをクリックしたときに除外/解除する処理
-const addResultClickToggle = (fighterBox, fighterIndex) => {
-    fighterBox.addEventListener("click", () => {
-        const imgDiv = fighterBox.querySelector(".imgBox");
-
-        if (bannedFighters.has(fighterIndex)) {
-            // 除外解除
-            bannedFighters.delete(fighterIndex);
-            imgDiv.classList.remove("clicked");
-
-            // 候補一覧も解除
-            if (fighterBoxes[fighterIndex]) {
-                fighterBoxes[fighterIndex].querySelector(".imgBox").classList.remove("clicked");
-            }
-        } else {
-            // 除外
-            bannedFighters.add(fighterIndex);
-            imgDiv.classList.add("clicked");
-
-            // 候補一覧にも反映
-            if (fighterBoxes[fighterIndex]) {
-                fighterBoxes[fighterIndex].querySelector(".imgBox").classList.add("clicked");
-            }
-        }
-
-        updateRemainingCount();
-        setCookie();
-    });
-};
 
 
 // historyを使う場合は，used class を付与
